df5c7bfd8d13c284b9d37f4ae05d0eea01ab14d8
Ashish Sharma <chemashish1611@gmail.com>
2024-01-29 10:33:34 +0530

    First Commit

---
+++ MyFirst.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+import java.util.*;
+
+class MyFirst
+{
+       public static void main(String arg[])
+       {
+            Scanner s = new Scanner(System.in);
+          //   int a,b,c;
+          //   System.out.println("Enter two numbers");
+          //   a = s.nextInt();
+          //   b = s.nextInt();
+          //   c =a+b;
+          //   System.err.println("Sum of two number is" + c);
+          String str = s.next();
+          System.err.println("Welcome "+str);
+
+       }
+   
+}
